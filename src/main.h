bb9a56dfbc206ab53bff7c79a2820eb4d32f91bb
Diablo Wu <wubo19842008@gmail.com>
2022-04-14 14:49:31 +0800

    init

---
+++ src/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+void getCityCode();
+void getCityWeater();
+void scrollBanner();
+void imgAnim();
+void weaterData(String *cityDZ, String *dataSK, String *dataFC);
+String week();
+String monthDa();
+void saveParamCallback();
+String monthDay();
+#endif
